a821c94e11bee554812e9da32f1ce97c17628ead
Steve Haapaoja <shaapaoja@gmail.com>
2015-12-04 22:39:39 -0800

    Add requirements for the project

---
+++ Product.py
@@ -0,0 +1,7 @@
+class Product:
+    def __init__(self, name, product_url, image_url, rating, votes):
+        self.name = name
+        self.product_url = product_url
+        self.image_url = image_url
+        self.rating = rating
+        self.votes = votes
