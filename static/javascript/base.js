92adb0905dc0903907974834483819657e1ddd82
Steve Haapaoja <shaapaoja@gmail.com>
2015-12-06 10:51:09 -0800

    Update stars back to value after vote

--- static/javascript/base.js
+++ static/javascript/base.js
@@ -28,17 +28,22 @@ var SUS = (function() {
     };
 
     var ratingClickHandler = function($this) {
-        console.debug("Rating clicked: ", $this);
-        console.debug("Rating clicked parent: ", $this.parent());
-
         var $parent = $this.parent();
         var cooldown = $parent.data('cooldown');
 
         if (cooldown === undefined || !cooldown) {
             console.log('Add rating');
-            $.post($this.data('posturl')).done(function() {
+            $.post($this.data('posturl')).done(function(data) {
                 console.debug('finished posting');
+                console.debug('Got data: ', data);
+                $parent.siblings('.jqueryStats').children('.jqueryStatsText').text(data['rating'].toFixed(2) + ' stars - ' + data['votes'] + ' votes');
                 $parent.data('cooldown', true);
+                console.log($this.is(':checked'));
+
+                setTimeout(function() {
+                    $parent.find('input[value="' + Math.round(data['rating']) + '"]').prop('checked', true);
+                }, 2000);
+
                 setTimeout(function() {
                     $parent.data('cooldown', false);
                 }, 5000);
