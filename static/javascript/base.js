86f0f340b10ece1fee65c314d3994234caaf3144
Steve Haapaoja <shaapaoja@gmail.com>
2015-12-06 14:26:34 -0800

    Removed borders; Added total votes

--- static/javascript/base.js
+++ static/javascript/base.js
@@ -42,9 +42,13 @@ var SUS = (function() {
             $.post($this.data('posturl')).done(function(data) {
                 console.debug('finished posting');
                 console.debug('Got data: ', data);
-                var $jqueryStatsText = $parent.siblings('.jqueryStats').children('.jqueryStatsText')
+                var $jqueryStatsText = $parent.siblings('.jqueryStats').children('.jqueryStatsText');
                 $jqueryStatsText.children('div').not('.jqueryProductName').each(function(index) {
-                    $(this).text(index + ' stars - ' + (data['votes_by_star'][index]/data['votes']).toFixed(2) * 100 + '%');
+                    if (index == 0) {
+                        $(this).text('Total votes - ' + data['votes']);
+                    } else {
+                        $(this).text(index + ' stars - ' + ((data['votes_by_star'][index-1]/data['votes']) * 100).toFixed(2) + '%');
+                    }
                 });
                 $parent.data('cooldown', true);
                 console.log($this.is(':checked'));
